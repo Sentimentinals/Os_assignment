49701f27b7541d26aa987a2eaf49579bd9c33639
Sentimentinals <139831686+Sentimentinals@users.noreply.github.com>
2025-11-20 23:31:00 +0700

    Update libmem.c

--- libmem.c
+++ libmem.c
@@ -178,6 +178,7 @@ int liballoc(struct pcb_t *proc, addr_t size, uint32_t reg_index)
   {
     return -1;
   }
+  printf("%s:%d\n",__func__,__LINE__);
 #ifdef IODUMP
   /* TODO dump IO content (if needed) */
 #ifdef PAGETBL_DUMP
